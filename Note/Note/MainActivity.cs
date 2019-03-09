551d6c32c51d8d48c713a62f6a9c0e86211e9f83
Risto Valge <risto@bristo.ee>
2019-03-09 14:06:02 +0200

    Tund

---
+++ Note/Note/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Note
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.test);
+
+            var count = 0;
+
+            var firstTextView = FindViewById<EditText>(Resource.Id.editText1);
+            var first1TextView = FindViewById<TextView>(Resource.Id.editText2);
+
+            var buttonTextView = FindViewById<Button>(Resource.Id.button1);
+
+
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+	}
+}
+
---
+++ Note/Note/NoteAdapter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace Note
+{
+    public class NoteAdapter : BaseAdapter
+    {
+
+        List<Note> items;
+        Activity context;
+
+        public NoteAdapter(Activity content, List<Note> items ) : base()
+        {
+            this.context = context;
+            this.items = items;
+        }
+        public override int Count
+        {
+            get { return items.Count; }
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long GetItemId(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
